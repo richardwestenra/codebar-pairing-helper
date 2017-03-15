abfbd013598843c51a61931697aa0843c5ad2144
Richard Westenra <rjwestenra@gmail.com>
2017-03-15 11:32:10 +0000

    Create a Row class and tidy up the jQuery

--- dist/pair.js
+++ dist/pair.js
@@ -1,6 +1,45 @@
 $(function() {
   console.log('pair.js loaded');
 
+  class Row {
+    constructor($row, i) {
+      this.$row = $row;
+      this.$name = $row.find('.small-5').find('a');
+      this.$desc = $row.find('.small-6');
+      this.$subject = this.$desc.children('small');
+      this.index = i;
+      this.id = this.getID();
+    }
+    getID() {
+      return this.$name.attr('href').match(/\d+/)[0];
+    }
+    getName() {
+      return this.$name.html().replace(/([\r\n])/gm,'');
+    }
+    getSubject() {
+      return this.$subject.text();
+    }
+    handle() {
+      return $('<div/>').attr('class','fa fa-navicon sortable-handle')
+    }
+    updateHTML() {
+      this.$row.data('id', this.id);
+      this.$row.find('.small-1').prepend(this.handle());
+      this.$desc.find('span.has-tip').remove();
+      this.$subject.attr('contenteditable', 'true').data('id', this.id);
+    }
+    updateData(data) {
+      const name = this.getName(),
+        subject = this.getSubject()
+      data[this.id] = {
+        id: this.id,
+        index: this.index,
+        name,
+        subject
+      };
+    }
+  }
+
   // const ORDER = ['java','python','ruby','version control', 'command line', 'javascript', 'js', 'html']
   var studentData = {};
 
@@ -12,25 +51,16 @@ $(function() {
 
   // Get the details of each student
   $row.each(function(i) {
-    var $name = $(this).find('.small-5').find('a');
-    var id = $name.attr('href').match(/\d+/)[0];
-    var name = $name.html();
-    var $desc = $(this).find('.small-6');
-    $desc.find('span.has-tip').remove();
-    var $subject = $desc.find('small');
-    var subject = $subject.text();
-    $subject.attr('contenteditable','true').on('keydown', function(){
-      console.log($(this).text());
-    });
-    $(this).find('.small-1').prepend(
-      $('<div/>').attr('class','fa fa-navicon sortable-handle')
-    );
-    studentData[id] = {
-      initialOrder: i,
-      currentOrder: i,
-      name,
-      subject
-    };
+    const row = new Row($(this), i);
+    row.updateHTML();
+    row.updateData(studentData);
+  });
+
+  // Update data when editing
+  $tbody.on('keydown', 'td.small-6 > small', function() {
+    const id = $(this).data('id');
+    studentData[id].subject = $(this).text();
+    console.log(studentData[id]);
   });
 
   // Reattach to the DOM
@@ -40,9 +70,7 @@ $(function() {
   var sortable = Sortable.create($tbody[0], {
     draggable: '.attendee',
     handle: '.sortable-handle',
-    onUpdate: function (evt){
-      console.log(evt);
-       var item = evt.item; // the current dragged HTMLElement
+    onUpdate: function(e) {
     }
   });
 });
