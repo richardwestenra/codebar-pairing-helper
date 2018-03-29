e836554829ab08b580252bc6bbb81028938c518f
Richard Westenra <rjwestenra@gmail.com>
2018-03-29 13:49:27 +0100

    Switch HTTP to HTTPS
    Now that codebar automatically redirects to HTTPS and my website is on HTTPS too, we can serve over HTTPS by default :)

--- dev/server.js
+++ dev/server.js
@@ -1,8 +1,8 @@
 const bookmarklet = require('bookmarklet');
 
-const script = `if (window.location.protocol === "https:") {
-  if (confirm("Scripts are likely blocked because this page is served over HTTPS. Change the protocol to HTTP?")) {
-    window.location.href = window.location.href.replace('https:','http:');
+const script = `if (window.location.protocol === "http:") {
+  if (confirm("Scripts are likely blocked because this page is served over HTTP. Change the protocol to HTTPS?")) {
+    window.location.href = window.location.href.replace('http:','https:');
   }
 } else {
   console.log("Let's pair!");
@@ -12,17 +12,17 @@ const makeBookmarklet = (domain) => bookmarklet.convert(
   script,
   {
     style: [
-      `http://${domain}/dist/pair.css`
+      `${domain}/dist/pair.css`
     ],
     script: [
-      `http://${domain}/dist/sortable.min.js`,
-      `http://${domain}/dist/pair.js`
+      `${domain}/dist/sortable.min.js`,
+      `${domain}/dist/pair.js`
     ]
   }
 );
 
-const live = makeBookmarklet('richardwestenra.com/codebar-pairing-helper');
-const local = makeBookmarklet('localhost:3000');
+const live = makeBookmarklet('https://richardwestenra.com/codebar-pairing-helper');
+const local = makeBookmarklet('http://localhost:3000');
 
 console.log('=== Live bookmarklet ===');
 console.log(live);
